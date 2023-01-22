13d992e0b577218ecc7384aa48ade2fc206364af
Jonas Almås <jonasalmaas7@gmail.com>
2023-01-22 18:28:51 +0100

    Fix bot only being able to connect one time

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+from fastapi import FastAPI, Request
 
 FACEIT_API_KEY = os.environ['FACEIT_API_KEY']
 FACEIT_PLAYER_ID = os.environ['FACEIT_PLAYER_ID']
@@ -10,17 +11,6 @@ from faceit_api import FaceitAPI
 from bot import DiscordBot
 
 faceit_api = FaceitAPI(FACEIT_API_KEY, FACEIT_PLAYER_ID, NUMBERS_OF_MATCHES)
-bot = DiscordBot(BOT_CHANNEL_ID, FACEIT_PLAYER_ID, NUMBERS_OF_MATCHES, faceit_api)
-
-
-@bot.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(bot.bot))
-    await bot.post_discord_message()
-    await bot.close()
-
-
-from fastapi import FastAPI, Request
 
 api = FastAPI()
 
@@ -32,4 +22,12 @@ async def root():
 
 @api.post("/onMatchFinished")
 async def onMatchFinished(request: Request):
+    bot = DiscordBot(BOT_CHANNEL_ID, FACEIT_PLAYER_ID, NUMBERS_OF_MATCHES, faceit_api)
+
+    @bot.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(bot.bot))
+        await bot.post_discord_message()
+        await bot.close()
+
     await bot.run(BOT_TOKEN)
